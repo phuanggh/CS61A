b592c176e681e0bd58b978f0cea95f4d0288f8bc
Penny Huang <pennyhuang@synology.com>
2024-05-03 17:51:13 +0800

    lab09: add answers

--- lab09/lab09.py
+++ lab09/lab09.py
@@ -1,3 +1,37 @@
+class Tree:
+    """
+    >>> t = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+    >>> t.label
+    3
+    >>> t.branches[0].label
+    2
+    >>> t.branches[1].is_leaf()
+    True
+    """
+    def __init__(self, label, branches=[]):
+        for b in branches:
+            assert isinstance(b, Tree)
+        self.label = label
+        self.branches = list(branches)
+
+    def is_leaf(self):
+        return not self.branches
+
+    def __repr__(self):
+        if self.branches:
+            branch_str = ', ' + repr(self.branches)
+        else:
+            branch_str = ''
+        return 'Tree({0}{1})'.format(self.label, branch_str)
+
+    def __str__(self):
+        def print_tree(t, indent=0):
+            tree_str = '  ' * indent + str(t.label) + "\n"
+            for b in t.branches:
+                tree_str += print_tree(b, indent + 1)
+            return tree_str
+        return print_tree(self).rstrip()
+
 def cumulative_mul(t):
     """Mutates t so that each node's label becomes the product of all labels in
     the corresponding subtree rooted at t.
@@ -12,7 +46,12 @@ def cumulative_mul(t):
     Tree(5040, [Tree(60, [Tree(3), Tree(4), Tree(5)]), Tree(42, [Tree(7)])])
     """
     "*** YOUR CODE HERE ***"
-
+    if t.branches is not []:
+        acc = t.label
+        for b in t.branches:
+            acc *= cumulative_mul(b).label
+        t.label = acc
+    return t
 
 def prune_small(t, n):
     """Prune the tree mutatively, keeping only the n branches
@@ -67,39 +106,3 @@ def delete(t, x):
         else:
             __________________________________
     t.branches = ___________________
-
-
-class Tree:
-    """
-    >>> t = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
-    >>> t.label
-    3
-    >>> t.branches[0].label
-    2
-    >>> t.branches[1].is_leaf()
-    True
-    """
-    def __init__(self, label, branches=[]):
-        for b in branches:
-            assert isinstance(b, Tree)
-        self.label = label
-        self.branches = list(branches)
-
-    def is_leaf(self):
-        return not self.branches
-
-    def __repr__(self):
-        if self.branches:
-            branch_str = ', ' + repr(self.branches)
-        else:
-            branch_str = ''
-        return 'Tree({0}{1})'.format(self.label, branch_str)
-
-    def __str__(self):
-        def print_tree(t, indent=0):
-            tree_str = '  ' * indent + str(t.label) + "\n"
-            for b in t.branches:
-                tree_str += print_tree(b, indent + 1)
-            return tree_str
-        return print_tree(self).rstrip()
-
