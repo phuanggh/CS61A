9cf1157904e79c1720538ce1d995515fe67e6398
Penny Huang <pennyhuang@synology.com>
2024-05-06 14:08:35 +0800

    lab10: add answers

--- lab10/lab10.scm
+++ lab10/lab10.scm
@@ -1,10 +1,23 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2)
+  (cond
+    ((< num1 num2) -1)
+    ((> num1 num2) 1)
+    (else 0)
+  )
+)
+ 
+(define (make-adder num) 
+  (lambda (inc) (+ num inc))
+)
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (composed f g)
+  (lambda (x) (f (g x)))
+)
 
-(define (composed f g) 'YOUR-CODE-HERE)
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n) 'YOUR-CODE-HERE
+  
+)
 
 (define (max a b)
   (if (> a b)
