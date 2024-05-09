35eed4ecac3321af89ab27de3124748dba5ef79c
Penny Huang <pennyhuang@synology.com>
2024-05-09 18:20:13 +0800

    hw07: add answers

--- hw07/hw07.scm
+++ hw07/hw07.scm
@@ -1,12 +1,31 @@
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp)
+  (if (= exp 0)
+    1
+    (if (= (remainder exp 2) 1)
+      (* base (pow base (- exp 1)))
+      (square (pow base (/ exp 2)))
+    )
+  )
+)
 
 (define (repeatedly-cube n x)
   (if (zero? n)
       x
-      (let (_________________)
-        (* y y y))))
+      (let ((y (repeatedly-cube (- n 1) x)))
+        (* y y y)
+      )
+  )
+)
+
+; def repeatedly-cube(n, x):
+;     if n == 0:
+;         return x
+;     else:
+;         y = repeatedly-cube(n - 1, x)
+;         return y * y * y
+  
 
 (define (cddr s) (cdr (cdr s)))
 
