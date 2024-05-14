3f95203c4c570b6b62ff25ccd3ac0dede0383afa
Penny Huang <pennyhuang@synology.com>
2024-05-14 10:20:48 +0800

    hw08: add answers

--- hw08/hw08.scm
+++ hw08/hw08.scm
@@ -1,7 +1,62 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s) 
+    ; if s is nil, return true
+    ; else compare the first two elements of s
+    ; if the first element is greater than the second element, return false
+    ; else call ascending? on the rest
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+    (if (null? s) 
+        #t
+        (if (> (car s) (cadr s))
+            #f
+            (ascending? (cdr s))
+        )
+    )    
+)
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (cadr s) 
+  (car (cdr s))
+)
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+
+(define (my-filter pred s)
+    ; if s is nil, return nil
+    ; else if pred is true for the first element of s, cons the first element of s to the result of calling my-filter on the rest of s
+    ; else call my-filter on the rest of s
+
+    (if (null? s)
+        '()
+        (if (pred (car s))
+            (cons (car s) (my-filter pred (cdr s)))
+            (my-filter pred (cdr s))
+        )
+    )
+
+)
+
+(define (interleave lst1 lst2) 
+    ; if lst1 is nil, return lst2
+    ; if lst2 is nil, return lst1
+    ; else cons the first element of lst1 to the result of calling interleave on lst2 and the rest of lst1
+
+    (if (null? lst1)
+        lst2
+        (if (null? lst2)
+            lst1
+            (cons (car lst1) (interleave lst2 (cdr lst1)))
+        )
+    )
+)
+
+(define (no-repeats s)
+    ; if s is nil, return nil
+    ; else if the first element of s is not in the rest of s, cons the first element of s to the result of calling no-repeats on the rest of s
+    ; else call no-repeats on the rest of s
+
+    (if (null? s)
+        '()
+        (if (not (member (car s) (cdr s)))
+            (cons (car s) (no-repeats (cdr s)))
+            (no-repeats (cdr s))
+        )
+    )
+)
